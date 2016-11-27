25666c9e05c4549ffa183f43c911768e92a1de3f
Peter Korsgaard <peter@korsgaard.com>
2016-11-27 21:11:49 +0100

    Revert "fs: call the pseudo wrapper"
    This reverts commit 74a3e75bb8208a8f3bb8426549fce1ea09b3be8a.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- fs/common.mk
+++ fs/common.mk
@@ -100,7 +100,7 @@ ifeq ($$(BR2_REPRODUCIBLE),y)
 endif
 	$$(call PRINTF,$$(ROOTFS_$(2)_CMD)) >> $$(FAKEROOT_SCRIPT)
 	chmod a+x $$(FAKEROOT_SCRIPT)
-	PATH=$$(BR_PATH) $$(HOST_DIR)/usr/bin/pseudo-wrapper -- $$(FAKEROOT_SCRIPT)
+	PATH=$$(BR_PATH) $$(HOST_DIR)/usr/bin/pseudo -- $$(FAKEROOT_SCRIPT)
 	$$(INSTALL) -m 0644 support/misc/target-dir-warning.txt $$(TARGET_DIR_WARNING_FILE)
 	-@rm -f $$(FAKEROOT_SCRIPT) $$(FULL_DEVICE_TABLE)
 ifneq ($$(ROOTFS_$(2)_COMPRESS_CMD),)
