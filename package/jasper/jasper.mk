9dd36263329e97f5917546a727f7843dfae8a819
Max Filippov <jcmvbkbc@gmail.com>
2016-11-28 13:54:30 -0800

    Revert "jasper: Disable debugging when building for xtensa"
    This reverts commit 71d9b0c1f06896f113b09e941aa84d979bff5710.
    Now that -mauto-litpools is in TARGET_ABI when building for xtensa, -O0
    builds succeed, so this workaround is no longer needed.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/jasper/jasper.mk
+++ package/jasper/jasper.mk
@@ -14,8 +14,6 @@ JASPER_LICENSE_FILES = LICENSE
 # No configure script included. We need to generate it.
 JASPER_AUTORECONF = YES
 
-JASPER_CONF_OPTS = --disable-strict
-
 # Xtensa gcc is unable to generate correct code with -O0 enabled by
 # --enable-debug. Allow package build but disable debug.
 ifeq ($(BR2_xtensa)$(BR2_ENABLE_DEBUG),yy)
