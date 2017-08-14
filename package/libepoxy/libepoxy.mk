9a687eb15a017ce5df63ccbb4637a0d58dc0fd12
Adrián Pérez de Castro <aperez@igalia.com>
2017-08-14 20:23:20 +0300

    libepoxy: bump to version 1.4.3
    Among other small fixes, the new version fixes crashes when running in
    X11 without GLX (i.e. using EGL). Additionally, the changes from patch
    "0002-Make-EGL-support-optional.patch" were committed upstream, and the
    file can be dropped.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libepoxy/libepoxy.mk
+++ package/libepoxy/libepoxy.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBEPOXY_VERSION_MAJOR = 1.4
-LIBEPOXY_VERSION = $(LIBEPOXY_VERSION_MAJOR).1
+LIBEPOXY_VERSION = $(LIBEPOXY_VERSION_MAJOR).3
 LIBEPOXY_SITE = http://ftp.gnome.org/pub/gnome/sources/libepoxy/$(LIBEPOXY_VERSION_MAJOR)
 LIBEPOXY_SOURCE = libepoxy-$(LIBEPOXY_VERSION).tar.xz
 LIBEPOXY_INSTALL_STAGING = YES
@@ -14,8 +14,6 @@ LIBEPOXY_AUTORECONF = YES
 LIBEPOXY_DEPENDENCIES = host-pkgconf xutil_util-macros
 LIBEPOXY_LICENSE = MIT
 LIBEPOXY_LICENSE_FILES = COPYING
-# 0002-Make-EGL-support-optional.patch
-LIBEPOXY_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
 LIBEPOXY_CONF_OPTS += --enable-egl
